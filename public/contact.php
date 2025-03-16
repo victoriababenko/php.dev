5ff55348d77b22e8e66b271e8b1d5577cf978039
victoriababenko <victoriababenko04@gmail.com>
2025-03-16 17:59:23 +0200

    php 01

---
+++ public/contact.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<h1>Contact page</h1>";
+
+echo "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui laborum commodi eius ex delectus id accusantium at, veritatis, eligendi distinctio facilis doloribus voluptatibus dolor quo rerum ad quae, provident esse.";
+
+echo date("F j, Y");
